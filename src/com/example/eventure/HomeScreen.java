f0dba8bd4ea9aa116f31390eb394fbc071c91ae4
JohnGarcia <JohnGarcia05@mail.fresnostate.edu>
2013-10-09 15:54:32 -0700

    Home Act to Library Act - John

--- src/com/example/eventure/HomeScreen.java
+++ src/com/example/eventure/HomeScreen.java
@@ -2,7 +2,9 @@ package com.example.eventure;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class HomeScreen extends Activity {
 
@@ -17,6 +19,10 @@ public class HomeScreen extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.home_screen, menu);
 		return true;
+	}
+	
+	public void toLibrary(View V){
+		startActivity(new Intent(HomeScreen.this, Library.class));
 	}
 
 }
