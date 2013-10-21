ebcb31bd230016a68426b65970a69870a0a204f0
JohnGarcia <JohnGarcia05@mail.fresnostate.edu>
2013-10-21 15:23:03 -0700

    Insertion sort added into the library java. - John

--- gen/com/example/eventure/R.java
+++ gen/com/example/eventure/R.java
@@ -26,9 +26,9 @@ public final class R {
         public static final int library_back=0x7f020002;
     }
     public static final class id {
-        public static final int Library=0x7f080000;
+        public static final int Library=0x7f080001;
         public static final int action_settings=0x7f080003;
-        public static final int glTest=0x7f080001;
+        public static final int glTest=0x7f080000;
         public static final int homeButtonFromLib=0x7f080002;
     }
     public static final class layout {
@@ -40,11 +40,11 @@ public final class R {
         public static final int library=0x7f070001;
     }
     public static final class string {
-        public static final int Home=0x7f050005;
-        public static final int Library=0x7f050002;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int glTest=0x7f050003;
+        public static final int home_button=0x7f050005;
+        public static final int library=0x7f050002;
         public static final int title_activity_library=0x7f050004;
     }
     public static final class style {
