d87806121e751a4d0dac53910bc9b94ff99d3960
Shon Saevang <shonsaevang@mail.fresnostate.edu>
2013-10-28 15:49:28 -0700

    Getters and setters added to story info

--- JAVAcode/StoryInfo.java
+++ JAVAcode/StoryInfo.java
@@ -4,4 +4,20 @@ public class StoryInfo {
 	private string title;
 	private string dateWritten;
 	
+	//setters and getters 
+	public string getAuthor(){return author;}
+	
+	public string getTitle(){return title;}
+	
+	public string getDateWritten(){return dateWritten;}
+	
+	private string setAuthor(string a){author=a;}
+	
+	private string setTitle(string t){title=t;}
+	
+	private string setDateWritten(string d){dateWritten=d;}
+	
+	
+	
+	
 }
