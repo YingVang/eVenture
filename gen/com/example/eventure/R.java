550200661dbe99139d121a0a33b70176ec8c6139
JohnGarcia <JohnGarcia05@mail.fresnostate.edu>
2013-11-03 11:12:50 -0800

    "Trying a new background for the home screen"
    -John

--- gen/com/example/eventure/R.java
+++ gen/com/example/eventure/R.java
@@ -22,8 +22,9 @@ public final class R {
     }
     public static final class drawable {
         public static final int background=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int library_back=0x7f020002;
+        public static final int background2=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int library_back=0x7f020003;
     }
     public static final class id {
         public static final int Library=0x7f080001;
