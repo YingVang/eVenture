b7d389b8811c6d740e67c4b75d981ac6c7e93892
Shon Saevang <shonsaevang@mail.fresnostate.edu>
2013-11-20 16:06:08 -0800

    Sort by title, author and date added to main class

--- JAVAcode/Main.java
+++ JAVAcode/Main.java
@@ -1,38 +1,82 @@
 import java.util.Vector;
 
+import com.example.eventure.Story;
+
 
 public class Main {
 
 	/**
 	 * @param args
 	 */
+	//Essentially the library of stories
+	Vector<Story> storylib = new Vector();
+	int j=storylib.size();	
+	
+	public void sortLibByTitle(){		
+		//insertion sorting		
+		Story temp;
+		Story temp2;
+	
+		for (int k = 1; k < j; k++){
+			//the object to compare with
+			temp = storylib.get(k);
+			for (int h = k-1; h >= 0; h--){
+				temp2 = storylib.get(h);	//the object to compare
+				//if temp2 is greater than temp than swap
+				if(temp2.getStoryInfo().getTitle() > temp.getStoryInfo().getTitle()){
+					storylib.set(h,temp2);	//essentially swapping objects
+					storylib.set(k,temp);	
+				}
+			//if not then decrement h	
+			}
+		} 
+	}
+	
+	public void sortLibByAuthor(){		
+		//insertion sorting		
+		Story temp;
+		Story temp2;
+	
+		for (int k = 1; k < j; k++){
+			//the object to compare with
+			temp = storylib.get(k);
+			for (int h = k-1; h >= 0; h--){
+				temp2 = storylib.get(h);	//the object to compare
+				//if temp2 is greater than temp than swap
+				if(temp2.getStoryInfo().getAuthor() > temp.getStoryInfo().getAuthor()){
+					storylib.set(h,temp2);	//essentially swapping objects
+					storylib.set(k,temp);	
+				}
+			//if not then decrement h	
+			}
+		} 
+	}
+	
+	public void sortLibByDate(){		
+		//insertion sorting		
+		Story temp;
+		Story temp2;
+	
+		for (int k = 1; k < j; k++){
+			//the object to compare with
+			temp = storylib.get(k);
+			for (int h = k-1; h >= 0; h--){
+				temp2 = storylib.get(h);	//the object to compare
+				//if temp2 is greater than temp than swap
+				if(temp2.getStoryInfo().getDateWritten() > temp.getStoryInfo().getDateWritten()){
+					storylib.set(h,temp2);	//essentially swapping objects
+					storylib.set(k,temp);	
+				}
+			//if not then decrement h	
+			}
+		} 
+	}
+	
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		//Essentially the library of stories
-		Vector<Story> storylib = new Vector<Story>();		
-		Integer j=storylib.size();	
-		
-		//these vectors will be used for the search bar
-		Vector<String> authVec = new Vector<String>();
-		for(Integer i=0; i<j; i++){
-			String auth = storylib.get(i).getStoryInfo().getAuthor();
-			authVec.add(auth);
-		}
-			
-		Vector<String> titleVec = new Vector<String>();
-		for(Integer x=0; x<j; x++){
-			String titl = storylib.get(x).getStoryInfo().getTitle();
-			titleVec.add(titl);
-		}
-		
-		Vector<Integer> dateVec = new Vector<Integer>();
-		for(Integer y=0; y<j; y++){
-			Integer dat = storylib.get(y).getStoryInfo().getDateWritten();
-			dateVec.add(dat);
-		}
-		 	
+
 	}
 	
 
