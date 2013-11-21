517d75b5cc95c61bb9b5f8b520eaafb2fa13c219
JohnGarcia <JohnGarcia05@mail.fresnostate.edu>
2013-11-20 19:59:25 -0800

    "animation home screen background, ground work." -John

--- gen/com/example/eventure/R.java
+++ gen/com/example/eventure/R.java
@@ -24,16 +24,18 @@ public final class R {
         public static final int background=0x7f020000;
         public static final int background2=0x7f020001;
         public static final int ic_launcher=0x7f020002;
-        public static final int library_back=0x7f020003;
-        public static final int white_background=0x7f020004;
+        public static final int img_list=0x7f020003;
+        public static final int library_back=0x7f020004;
+        public static final int white_background=0x7f020005;
     }
     public static final class id {
-        public static final int Library=0x7f080000;
-        public static final int action_library=0x7f080003;
-        public static final int action_search=0x7f080004;
-        public static final int action_settings=0x7f080005;
-        public static final int continue_button=0x7f080001;
-        public static final int glSurface=0x7f080002;
+        public static final int Library=0x7f080001;
+        public static final int action_library=0x7f080004;
+        public static final int action_search=0x7f080005;
+        public static final int action_settings=0x7f080006;
+        public static final int continue_button=0x7f080002;
+        public static final int gif_img=0x7f080000;
+        public static final int glSurface=0x7f080003;
     }
     public static final class layout {
         public static final int activity_home_screen=0x7f030000;
--- src/com/example/eventure/HomeScreen.java
+++ src/com/example/eventure/HomeScreen.java
@@ -2,11 +2,13 @@ package com.example.eventure;
 
 //test comment
 
-import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.ImageView;
 
 public class HomeScreen extends Activity {
 
@@ -14,6 +16,18 @@ public class HomeScreen extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home_screen);
+		
+		// Load the ImageView that will host the animation and
+		// set its background to our AnimationDrawable XML resource.
+		ImageView img = (ImageView)findViewById(R.id.gif_img);
+		img.setBackgroundResource(R.drawable.img_list);
+
+		// Get the background, which has been compiled to an AnimationDrawable object.
+		AnimationDrawable frameAnimation = (AnimationDrawable) img.getBackground();
+
+		// Start the animation (looped playback by default).
+		frameAnimation.start();
+		
 	}
 
 	@Override
